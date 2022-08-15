c2321e1197470ed5a7db43bb9a9aaa6948481d79
koreacat <kj9966@naver.com>
2022-08-15 20:13:58 +0900

    threejs

---
+++ 02/basic/src/main.js
@@ -0,0 +1,6 @@
+import * as THREE from 'three';
+
+const canvas = document.querySelector('#three-canvas');
+const renderer = new THREE.WebGLRenderer({ canvas });
+
+renderer.setSize(window.innerWidth, window.innerHeight);
